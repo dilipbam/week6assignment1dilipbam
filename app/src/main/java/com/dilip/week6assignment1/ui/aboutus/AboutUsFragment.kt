c2fe7731b970ec2cdec2de36cdfbd6073aedee25
dilipbam <57613211+dilipbam@users.noreply.github.com>
2021-01-17 21:31:41 +0545

    Design for fragments has been done

--- app/src/main/java/com/dilip/week6assignment1/ui/aboutus/AboutUsFragment.kt
+++ app/src/main/java/com/dilip/week6assignment1/ui/aboutus/AboutUsFragment.kt
@@ -1,4 +1,4 @@
-package com.dilip.week6assignment1.ui.notifications
+package com.dilip.week6assignment1.ui.aboutus
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.dilip.week6assignment1.R
 
-class NotificationsFragment : Fragment() {
+class AboutUsFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var aboutUsViewModel: AboutUsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        aboutUsViewModel =
+            ViewModelProvider(this).get(AboutUsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_aboutus, container, false)
         val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+        aboutUsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/dilip/week6assignment1/ui/aboutus/AboutUsViewModel.kt
+++ app/src/main/java/com/dilip/week6assignment1/ui/aboutus/AboutUsViewModel.kt
@@ -1,10 +1,10 @@
-package com.dilip.week6assignment1.ui.notifications
+package com.dilip.week6assignment1.ui.aboutus
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class AboutUsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
